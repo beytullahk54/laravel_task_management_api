a74e048f1ae873e6fe9d4230fd00995d68b08ba8
beytullah <kahramanbey54@gmail.com>
2025-08-12 23:41:30 +0300

    Temel endpointlerin eklenmesi

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\General\LoginController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\TaskController;
 
 Route::post('/login', [LoginController::class, 'login']);
+
+Route::apiResource('teams', TeamController::class)->middleware('auth:sanctum');
+Route::apiResource('tasks', TaskController::class)->middleware('auth:sanctum');
